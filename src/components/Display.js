5b1740edbe04e1d88c5cfea3705de00d788f7fd1
Daijon Bereola-Carson <54553857+daijonbereola@users.noreply.github.com>
2022-11-09 20:17:18 -0800

    Styling Components
    added style to JS components

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { StyledDisplay } from './styles/StyledDisplay'
 
 export default function Display({ gameOver, text }) {
   return (
-    <div>{text}</div>
+    <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
   )
 }
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,18 +3,23 @@ import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
 
+import { createStage } from '../gameHelpers'
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
+
 export default function Tetris() {
   return (
-    <div>
-        <Stage />
-        <aside>
-            <div>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-            </div>
-            <StartButton />
-        </aside>
-    </div>
+    <StyledTetrisWrapper>
+        <StyledTetris>
+            <Stage stage={createStage()}/>
+            <aside>
+                <div>
+                    <Display text="Score" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                </div>
+                <StartButton />
+            </aside>
+        </StyledTetris>
+    </StyledTetrisWrapper>
   )
 }
